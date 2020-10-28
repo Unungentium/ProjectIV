be0d54035b294ad2ce9cdd52ed648f1557aec647
Unungentium <franciszek.pauwels@durham.ac.uk>
2020-10-28 09:47:31 +0000

    added a missing constant to the cc file

--- QtoQBcSplitting.cc
+++ QtoQBcSplitting.cc
@@ -18,7 +18,7 @@
 using namespace Herwig;
 
 QtoQBcSplitting::QtoQBcSplitting() {}
-
+const double  QtoQBcSplitting::v=1.;
 QtoQBcSplitting::~QtoQBcSplitting() {}
 
 IBPtr QtoQBcSplitting::clone() const {
